78b989bda4983eb7424b5413c756541e58abd782
Naomi Katz <naykatz@gmail.com>
2019-02-17 07:21:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,9 @@ def key_for_min_value(name_hash)
     return nil
   else
     #create an array to store all values
-  value_array = []
-    name_hash.each do |key, value|
-      value_array << value 
+  #value_array = []
+    #name_hash.each do |key, value|
+      #value_array << value 
+      comp_value = name_hash[]
   end
 end
